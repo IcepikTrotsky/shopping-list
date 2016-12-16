55cea2f10a45a119f098778274d921b0eaf88896
IcepikTrotsky <luke.boyes@gmail.com>
2016-12-16 15:21:24 +1100

    main-loop-change
    Changed main loop to infinite loop with a break statement rather than a boolean variable to exit loop. Should make things clearer and less likely to cause issues in the future.

--- shoppingApp.py
+++ shoppingApp.py
@@ -1,17 +1,15 @@
-running = True
 shopping_list = []
 
 print("Enter you shopping list items one at a time")
 print("Enter 'DONE' to exit and display your list")
 
-while running:
+while True:
 	
 	new_item = input("> ")
 
 	if new_item == 'DONE':
-		running = False
-
 		for item in shopping_list:
 			print(item)
+		break
 	else:
 		shopping_list.append(new_item)
